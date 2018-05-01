6cd528e991fc32a1284a794ca247da46fe768bce
Billy Ferguson <william.d.ferg@gmail.com>
2018-05-01 17:54:10 +0000

    Update list.py

--- deelist/utils/list.py
+++ deelist/utils/list.py
@@ -2,35 +2,38 @@ from builtins import object
 import requests
 
 class ListWrapper(object):
-    def __init__(self, base_url, token, header):
+    def __init__(self, base_url):
         self.base_url = base_url
-        self.token = token
-        self.header = header
-        
-    def get_lists(self):
+    
+    def header(token):
+        HEADER = {"Accept": "application/json",
+                  "Authorization": "Bearer {}".format(token)}
+        return HEADER
+    
+    def get_lists(self, token):
         r = requests.get(self.base_url, 
-                         headers=self.header)
+                         headers=header(token))
         if r.status_code == 200:
             return(r.json())
     
-    def get_shopping_list_id(self):
-        list_metadeta = self.get_lists()
+    def get_shopping_list_id(self, token):
+        list_metadeta = self.get_lists(token)
         list_id = ""
         for list in list_metadata["lists"]:
             if list["name"] == "Alexa shopping list":
                 list_id = list["listId"]
         return list_id
         
-    def get_shopping_list(self):
+    def get_shopping_list(self, token):
         URL = self.base_url + \
-              self.get_shopping_list_id() + \
+              self.get_shopping_list_id(token) + \
               "/active"
-        r = requests.get(URL, headers=self.header)
+        r = requests.get(URL, headers=header(token))
         if r.status_code == 200:
             return(r.json())
 
-    def shopping_list_items(self):
-        shopping_list = self.get_shopping_list()
+    def shopping_list_items(self, token):
+        shopping_list = self.get_shopping_list(token)
         if shopping_list == None:
             return []
         items = []
@@ -39,7 +42,7 @@ class ListWrapper(object):
                 items.append(item["value"])
         return items
     
-    def delete_item_in_shopping_list(self, item_id):
-        URL = self.base_url + self.get_shopping_list_id() + \
+    def delete_item_in_shopping_list(self, item_id, token):
+        URL = self.base_url + self.get_shopping_list_id(token) + \
               "/items/" + item_id
-        return requests.delete(URL, headers=self.header)
+        return requests.delete(URL, headers=header(token))
